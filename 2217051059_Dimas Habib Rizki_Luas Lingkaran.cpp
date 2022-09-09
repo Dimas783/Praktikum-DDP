e07a8b2d89d9c38e950886629df8d871a13bf6b1
Dimas Habib Rizki <112569594+Dimas783@users.noreply.github.com>
2022-09-09 14:37:37 +0700

    Add files via upload

---
+++ 2217051059_Dimas Habib Rizki_Luas Lingkaran.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+  system("color c");
+  float r, rumus;
+  cout<<"Input jari-jari lingkaran: ";
+  cin>>r;
+  rumus = 3.14*r*r;
+  cout<<"Luas lingkaran = "<<rumus<< endl;
+  return 0;
+}
